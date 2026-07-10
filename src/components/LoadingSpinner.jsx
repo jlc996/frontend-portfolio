35e50ea6f12b612d61d77282605dfdee037a774f
Joshua Craven <josh96craven@gmail.com>
2026-07-10 09:39:28 -0500

    Basic reusable loading message

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+// LoadingSpinner.jsx
+
+function LoadingSpinner() {
+  return (
+    <div className="loading-spinner">
+      <h2>Loading...</h2>
+    </div>
+  );
+}
+
+export default LoadingSpinner;
